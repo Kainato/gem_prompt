21dfb371a99ab7af94d4f3e421db6a4b4c09db0f
Kainato <caiocaladaraujo@gmail.com>
2025-10-02 19:14:17 -0300

    Refatorar: adicionar suporte a padding no WdScaffold e ajustar layout do PromptForm

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -115,69 +115,67 @@ class PromptFormState extends State<PromptForm> {
           },
         ),
       ],
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: FutureBuilder(
-                    future: GeminiService.listarModelos(apiKey),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return LinearProgressIndicator();
-                      } else if (snapshot.hasError) {
-                        return Text("Erro ao carregar modelos");
-                      } else {
-                        return DropdownMenu(
-                          menuHeight: MediaQuery.of(context).size.height * 0.4,
-                          dropdownMenuEntries: GeminiService.dropdownModelos,
-                          expandedInsets: EdgeInsets.all(0),
-                          enableSearch: false,
-                          enableFilter: false,
-                          label: Text("Selecione o modelo de IA"),
-                          controller: GeminiService.selectedModel.$1,
-                          onSelected: (value) {
-                            if (value != null) {
-                              GeminiService.selectedModel.$2.value = value;
-                            }
-                          },
-                        );
-                      }
-                    },
-                  ),
-                ),
-                buildTextField("Papel/Especialista", _papelController),
-                buildTextField("Contexto", _contextoController),
-                buildTextField("Objetivo", _objetivoController),
-                buildTextField("Detalhes/Regras", _detalhesController),
-                buildTextField("Formato da resposta", _formatoController),
-                buildTextField("Tom/Estilo", _tomController),
-                buildTextField("Exemplo/Referência", _exemploController),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      gerarPromptEChamarIA();
+      padding: EdgeInsets.zero,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: FutureBuilder(
+                  future: GeminiService.listarModelos(apiKey),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return LinearProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text("Erro ao carregar modelos");
+                    } else {
+                      return DropdownMenu(
+                        menuHeight: MediaQuery.of(context).size.height * 0.4,
+                        dropdownMenuEntries: GeminiService.dropdownModelos,
+                        expandedInsets: EdgeInsets.all(0),
+                        enableSearch: false,
+                        enableFilter: false,
+                        label: Text("Selecione o modelo de IA"),
+                        controller: GeminiService.selectedModel.$1,
+                        onSelected: (value) {
+                          if (value != null) {
+                            GeminiService.selectedModel.$2.value = value;
+                          }
+                        },
+                      );
                     }
                   },
-                  child: Text("Gerar Prompt e Consultar IA"),
                 ),
-                SizedBox(height: 20),
-                if (carregando) CircularProgressIndicator(),
-                if (respostaIA != null)
-                  Card(
-                    child: Markdown(
-                      data: respostaIA!,
-                      selectable: true,
-                      shrinkWrap: true,
-                    ),
+              ),
+              buildTextField("Papel/Especialista", _papelController),
+              buildTextField("Contexto", _contextoController),
+              buildTextField("Objetivo", _objetivoController),
+              buildTextField("Detalhes/Regras", _detalhesController),
+              buildTextField("Formato da resposta", _formatoController),
+              buildTextField("Tom/Estilo", _tomController),
+              buildTextField("Exemplo/Referência", _exemploController),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    gerarPromptEChamarIA();
+                  }
+                },
+                child: Text("Gerar Prompt e Consultar IA"),
+              ),
+              SizedBox(height: 20),
+              if (carregando) CircularProgressIndicator(),
+              if (respostaIA != null)
+                Card(
+                  child: Markdown(
+                    data: respostaIA!,
+                    selectable: true,
+                    shrinkWrap: true,
                   ),
-              ],
-            ),
+                ),
+            ],
           ),
         ),
       ),
--- lib/widgets/layout/wd_scaffold.dart
+++ lib/widgets/layout/wd_scaffold.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 
 class WdScaffold extends StatelessWidget {
   final Widget body;
+  final EdgeInsetsGeometry? padding ;
   final List<Widget>? actions;
-  const WdScaffold({super.key, required this.body, this.actions});
+  const WdScaffold({super.key, required this.body, this.actions, this.padding});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: const Text('Gerador de Prompts IA'), actions: actions),
-        body: body,
+        body: Padding(
+          padding: padding ?? const EdgeInsets.all(16.0),
+          child: body,
+        ),
       ),
     );
   }
