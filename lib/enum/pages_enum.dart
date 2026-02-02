b6fb90154be0a8f7dd18c3b7e6a0f13c99c35512
Kainato <caiocaladaraujo@gmail.com>
2026-02-02 16:40:02 -0300

    Revert "Add Screenshot Assistant feature with Gemini integration"
    This reverts commit 8bde5d8ca88a9c328903290b96d5a75d7980730f.

--- lib/enum/pages_enum.dart
+++ lib/enum/pages_enum.dart
@@ -4,9 +4,8 @@ import '../features/home/home_page.dart';
 import '../features/padrao/prompt_padrao_page.dart';
 import '../features/settings/settings_page.dart';
 import '../features/task_creator/task_creator_page.dart';
-import '../features/screenshot_assistant/screenshot_assistant_page.dart';
 
-enum PagesEnum { home, padrao, task, screenshotAssistant, settings }
+enum PagesEnum { home, padrao, task, settings }
 
 extension PagesEnumExtension on PagesEnum {
   String get title {
@@ -17,8 +16,6 @@ extension PagesEnumExtension on PagesEnum {
         return 'Prompt Padrão';
       case PagesEnum.task:
         return 'Reformulador de "Tasks"';
-      case PagesEnum.screenshotAssistant:
-        return 'Screenshot Assistant';
       case PagesEnum.settings:
         return 'Configurações';
     }
@@ -32,8 +29,6 @@ extension PagesEnumExtension on PagesEnum {
         return Icons.text_fields;
       case PagesEnum.task:
         return Icons.task;
-      case PagesEnum.screenshotAssistant:
-        return Icons.screenshot;
       case PagesEnum.settings:
         return Icons.settings;
     }
@@ -47,8 +42,6 @@ extension PagesEnumExtension on PagesEnum {
         return ('/padrao', const PromptPadraoPage());
       case PagesEnum.task:
         return ('/task', const TaskCreatorPage());
-      case PagesEnum.screenshotAssistant:
-        return ('/screenshot-assistant', const ScreenshotAssistantPage());
       case PagesEnum.settings:
         return ('/settings', const SettingsPage());
     }
@@ -61,8 +54,6 @@ extension PagesEnumExtension on PagesEnum {
       case PagesEnum.padrao:
         return true;
       case PagesEnum.task:
-        return true;
-      case PagesEnum.screenshotAssistant:
         return true;
       case PagesEnum.settings:
         return true;
